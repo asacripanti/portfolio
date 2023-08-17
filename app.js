708cb9f417e5a0e4678f4fd6bde46be547be2212
Alejandro Sacripanti <asacripanti93@gmail.com>
2023-08-17 13:44:44 -0500

    first version

---
+++ app.js
@@ -0,0 +1,13 @@
+const flowElements = document.querySelectorAll('.grid-even-columns .flow');
+
+flowElements.forEach(flow => {
+    flow.addEventListener('mouseenter', () => {
+        flow.classList.add('transition-active');
+    });
+
+    flow.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            flow.classList.remove('transition-active');
+        }, 1000); // Adjust the delay as needed
+    });
+});
